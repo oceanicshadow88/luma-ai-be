300d8205caf47911a5dff0df0272abd840478ed2
Cristine <cristineypj@gmail.com>
2025-05-07 00:06:45 +1000

    feat:password hash

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,54 +1,54 @@
 // authentication, authorization
 import { Request, Response, NextFunction } from 'express';
-import UserModel from '../models/user'
+import UserModel from '../models/user';
 
 export const register = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const {
-            username,
-            password,
-            email,
-            avatarUrl,
-            locale,
-        }: { username: string; password: string; email: string; avatarUrl?: string; locale: string } =
-            req.body;
-        const user = new UserModel({
-            username,
-            password,
-            email,
-            avatarUrl,
-            locale,
-        });
+  try {
+    const {
+      username,
+      password,
+      email,
+      avatarUrl,
+      locale,
+    }: { username: string; password: string; email: string; avatarUrl?: string; locale: string } =
+      req.body;
+    const user = new UserModel({
+      username,
+      password,
+      email,
+      avatarUrl,
+      locale,
+    });
 
-        await user.hashPassword();
-        await user.save();
+    await user.hashPassword();
+    await user.save();
 
-        res.status(201).json(user);
-    } catch (error) {
-        return next(error);
-    }
+    res.status(201).json(user);
+  } catch (error) {
+    return next(error);
+  }
 };
 
 export const login = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const { email, password }: { email: string; password: string } = req.body;
+  try {
+    const { email, password }: { email: string; password: string } = req.body;
 
-        const existingUser = await UserModel.findOne({ email }).exec();
-        if (!existingUser) {
-            res.status(401).json({ error: 'No account found with this email address' });
-            return;
-        }
-
-        // Check password
-        const validatePassword: boolean = await existingUser.validatePassword(password);
-        if (!validatePassword) {
-            // fail fast
-            res.status(401).json({ error: 'Invalid credentials' });
-            return;
-        }
-
-        res.json({ email: existingUser.email });
-    } catch (error) {
-        return next(error);
+    const existingUser = await UserModel.findOne({ email }).exec();
+    if (!existingUser) {
+      res.status(401).json({ error: 'No account found with this email address' });
+      return;
     }
+
+    // Check password
+    const validatePassword: boolean = await existingUser.validatePassword(password);
+    if (validatePassword) {
+      // fail fast
+      res.status(401).json({ error: 'Invalid credentials' });
+      return;
+    }
+
+    res.json({ email: existingUser.email });
+  } catch (error) {
+    return next(error);
+  }
 };
--- src/models/user.ts
+++ src/models/user.ts
@@ -2,57 +2,60 @@ import mongoose, { Document, Schema, Model } from 'mongoose';
 import bcrypt from 'bcryptjs';
 
 export interface IUser extends Document {
-    username: string;
-    password: string;
-    email: string;
-    avatarUrl: string;
-    locale: string;
-    createdAt: Date;
-    active: boolean;
-    hashPassword(): Promise<void>;
-    validatePassword(inputPassword: string): Promise<boolean>;
+  username: string;
+  password: string;
+  email: string;
+  avatarUrl: string;
+  locale: string;
+  createdAt: Date;
+  active: boolean;
+  hashPassword(): Promise<void>;
+  validatePassword(inputPassword: string): Promise<boolean>;
 }
 
 const userSchema: Schema<IUser> = new Schema(
-    {
-        username: {
-            type: String,
-            required: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        avatarUrl: {
-            type: String,
-        },
-        locale: {
-            type: String,
-            required: true,
-            default: 'en',
-        },
-        active: {
-            type: Boolean,
-            required: true,
-            default: true,
-        }
+  {
+    username: {
+      type: String,
+      required: true,
     },
-    { timestamps: true },
+    password: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    avatarUrl: {
+      type: String,
+    },
+    locale: {
+      type: String,
+      required: true,
+      default: 'en',
+    },
+    active: {
+      type: Boolean,
+      required: true,
+      default: true,
+    },
+  },
+  { timestamps: true },
 );
 
 // use this: no arrow function
 userSchema.methods.hashPassword = async function (this: IUser): Promise<void> {
-    this.password = await bcrypt.hash(this.password, 12);
-}
+  this.password = await bcrypt.hash(this.password, 12);
+};
 
-userSchema.methods.validatePassword = async function (this: IUser, password: string): Promise<void> {
-    bcrypt.compare(password, this.password)
-}
+userSchema.methods.validatePassword = async function (
+  this: IUser,
+  password: string,
+): Promise<void> {
+  bcrypt.compare(password, this.password);
+};
 
 // Prevent duplicate model registration in development (hot reload)
 const UserModel: Model<IUser> = mongoose.models.User || mongoose.model<IUser>('User', userSchema);
