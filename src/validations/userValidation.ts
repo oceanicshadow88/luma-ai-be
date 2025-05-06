94c348e6facedd0ef40ec569e04bed1847baff61
Cristine <cristineypj@gmail.com>
2025-05-06 22:08:02 +1000

    fix: edit appError.ts file name

--- src/validations/userValidation.ts
+++ src/validations/userValidation.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { AppError } from '../error/appError';
+import { AppError } from '../error/aappError';
 
 /**
  * Validates user input for creating or updating a user
