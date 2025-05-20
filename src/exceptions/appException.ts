4d80c947b073da035b8e6bef1169adcfc0f76192
Cristine-YPJ <cristineypj@gmail.com>
2025-05-20 22:49:37 +1000

    Update appException.ts
    Remove unnecessary error exceptions

--- src/exceptions/appException.ts
+++ src/exceptions/appException.ts
@@ -2,14 +2,15 @@ import type { SafePayload } from '../config';
 
 class AppException extends Error {
   statusCode: number;
-  payload: SafePayload;
+  payload?: SafePayload;
 
-  constructor(statusCode = 500, message: string, payload?: SafePayload) {
+  constructor(statusCode: number, message: string, payload?: SafePayload) {
     super(message);
-    this.name = new.target.name;
     this.statusCode = statusCode;
-    this.payload = payload || {};
-    Object.setPrototypeOf(this, new.target.prototype);
+    this.payload = payload;
+
+    // make sure instandof work in old environment
+    Object.setPrototypeOf(this, AppException.prototype);
   }
 }
 
