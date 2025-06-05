e23f2063b2657d268082a243b98a3a929c7c5c3a
Cristine <cristineypj@gmail.com>
2025-06-05 22:21:41 +1000

    feat: user login, check slug with url subdomain

--- src/controllers/auth/loginController.ts
+++ src/controllers/auth/loginController.ts
@@ -1,17 +1,24 @@
 import { Request, Response } from 'express';
-import { loginService } from '../../services/auth/loginService';
+import { LoginResult, loginService } from '../../services/auth/loginService';
+import { extractSubdomain } from '../../lib/extractSubdomain';
 
 export const login = async (req: Request, res: Response) => {
   // get data from request body
   const { email, password }: { email: string; password: string } = req.body;
   // call user service to do DB operation
-  const { refreshToken, accessToken, membership } = await loginService.login({
+  // const { refreshToken, accessToken, membership } = await loginService.login({
+  //   email,
+  //   password,
+  // });
+  const slug = await extractSubdomain(req);
+  const loginResult: LoginResult = await loginService.userlogin({
     email,
     password,
+    slug,
   });
 
   res.json({
     success: true,
-    data: { refreshToken, accessToken, membership },
+    data: loginResult,
   });
 };
