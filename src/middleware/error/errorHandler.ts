8d67fd1f06e8a52f45144dc90d03643bed16144e
Cristine-YPJ <cristineypj@gmail.com>
2025-06-06 21:40:26 +1000

    Update errorHandler.ts

--- src/middleware/error/errorHandler.ts
+++ src/middleware/error/errorHandler.ts
@@ -54,6 +54,9 @@ const errorHandler: ErrorRequestHandler = (
       case 'LIMIT_UNEXPECTED_FILE':
         message = 'Invalid file type';
         break;
+      default: 
+        Message= 'File upload error';
+        break;
     }
 
     res.status(400).json({ message });
