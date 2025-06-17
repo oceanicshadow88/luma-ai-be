da808b32f819a51106348ca2704a3246dbcf057c
kitman <kitman200220022002@gmail.com>
2025-06-17 23:47:50 +1000

    la-64: add user in invite api

--- src/services/invitationService.ts
+++ src/services/invitationService.ts
@@ -1,12 +1,38 @@
 import { generateInvitationLink } from '../utils/invitationLink';
 import { GenerateInvitationRequest, GenerateInvitationResponse } from '../types/invitation';
-import { EXPIRES_TIME_CONFIG } from '../config';
+import { EXPIRES_TIME_CONFIG, MEMBERSHIP_STATUS, ROLE } from '../config';
+import { RegisterUserInput } from '../controllers/auth/registerController';
+import { userService } from './userService';
+import { membershipService } from './membershipService';
+
+const createUserAndTokens = async (userInput: RegisterUserInput) => {
+  const newUser = await userService.createUser(userInput);
+  // Generate authentication tokens
+  const { refreshToken, accessToken } = await newUser.generateTokens();
+  await userService.updateUserById(newUser.id, { refreshToken });
+
+  return { newUser, refreshToken, accessToken };
+};
 
 export class InvitationService {
-  static async generateInvitation({
-    email,
-    role,
-  }: GenerateInvitationRequest): Promise<GenerateInvitationResponse> {
+  static async generateInvitation(
+    { email, role }: GenerateInvitationRequest,
+    companyId: any,
+  ): Promise<GenerateInvitationResponse> {
+    const { newUser } = await createUserAndTokens({
+      email,
+      password: 'any',
+      username: 'teacher5',
+      firstName: 'teacher',
+      lastName: 'teacher',
+      verifyValue: '66666',
+    });
+    await membershipService.createMembership({
+      company: companyId,
+      user: newUser._id as any,
+      role: ROLE.INSTRUCTOR,
+      status: MEMBERSHIP_STATUS.INVITED,
+    });
     const invitationLink = await generateInvitationLink(email, role);
 
     return {
