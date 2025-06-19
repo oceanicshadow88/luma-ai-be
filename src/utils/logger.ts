c470056ce9e7bda15d42a705616d2ab74fc3773a
Cristine <cristineypj@gmail.com>
2025-06-19 21:22:39 +1000

    bugfix: error message and refactor logger color hint

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,23 +1,33 @@
-import winston from 'winston';
+import chalk from 'chalk';
 import path from 'path';
+import winston from 'winston';
+
 import config from '../config';
 
 export const createLogger = (filename?: string): winston.Logger => {
   const logger = winston.createLogger({
-    level: config.logLevel,
+    level: config.logLevel || 'info',
     defaultMeta: {
       filename: filename ? path.basename(String(filename)) : undefined,
     },
     format: winston.format.combine(
       winston.format.timestamp(),
-      winston.format.printf(({ timestamp, filename, level, message, payload }) => {
+      winston.format.printf(({ timestamp, level, message, filename, ...meta }) => {
         const fileInfo = typeof filename === 'string' ? `[${filename}]` : '';
-        const payloadInfo = payload ? `\n${JSON.stringify(payload, null, 2)}` : '';
-        return `[${timestamp}][${level}] ${fileInfo}: ${message}${payloadInfo}`;
+        const payloadInfo =
+          meta && Object.keys(meta).length ? `\n${JSON.stringify(meta, null, 2)}` : '';
+        return `[${timestamp}][${level.toUpperCase()}] ${fileInfo} ${message}${payloadInfo}`;
       }),
     ),
     transports: [
-      new winston.transports.Console(),
+      new winston.transports.Console({
+        format: winston.format.printf(({ level, message }) => {
+          if (level === 'error') return chalk.red(`[ERROR] ${message}`);
+          if (level === 'warn') return chalk.yellow(`[WARN] ${message}`);
+          if (level === 'info') return chalk.blue(`[INFO] ${message}`);
+          return chalk.white(`[${level.toUpperCase()}] ${message}`); // fallback for other levels
+        }),
+      }),
       new winston.transports.File({
         filename: 'logs/error.log',
         level: 'error',
