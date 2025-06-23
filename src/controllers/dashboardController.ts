94afd3c0aadc0843899e38115e6248d5b54d0ba7
Cristine-YPJ <cristineypj@gmail.com>
2025-06-23 23:24:45 +1000

    lum-3-4 resolve merge conflict

--- src/controllers/dashboardController.ts
+++ src/controllers/dashboardController.ts
@@ -1,20 +1,26 @@
 import { Request, Response } from 'express';
 
+import { adminDashboardDataMock, DEFAULT_MOCK_COUNT } from '../mockData/adminDashboardDataMock';
+import { companyUsageService } from '../services/companyUsageService';
 import { userService } from '../services/userService';
 
 export const dashboardController = {
-  getAdminDashboardData: async (req: Request, res: Response) => {
+  getAdminDashboard: async (req: Request, res: Response) => {
     if (!req.user?.id) {
-      return res.status(401).json({ success: false, message: 'Unauthorized' });
+      return res.status(401).json({ success: false, message: 'Unauthorized User' });
     }
     if (!req.companyId) {
       return res.status(401).json({ success: false, message: 'Missing company' });
     }
 
+    const companyUsage = await companyUsageService.upsertCompanyUsage(req.companyId);
     const currentUserInfo = await userService.getCurrentUserInfo(req.user.id, req.companyId);
 
     const dashboardData = {
       currentUserInfo,
+      totalLearners: companyUsage?.currentLearners ?? DEFAULT_MOCK_COUNT,
+      totalInstructors: companyUsage?.currentInstructors ?? DEFAULT_MOCK_COUNT,
+      adminDashboardDataMock,
     };
 
     res.status(200).json({ success: true, data: dashboardData });
