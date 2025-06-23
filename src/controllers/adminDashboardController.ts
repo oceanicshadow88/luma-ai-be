6e467b80f01688ca3a637056f24d336dc8be93f5
Cristine <64547045+Ivy-Jiao@users.noreply.github.com>
2025-06-23 20:05:04 +1000

    feat: lum-3-2 count number of people with role in given company

---
+++ src/controllers/adminDashboardController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+
+import { adminDashboardDataMock, DEFAULT_MOCK_COUNT } from '../mockData/adminDashboardDataMock';
+import { companyUsageService } from '../services/companyUsageService';
+
+export const adminDashboardController = {
+  getAdminDashboardData: async (req: Request, res: Response) => {
+    if (!req.user?.id) {
+      return res.status(401).json({ success: false, message: 'Unauthorized' });
+    }
+    if (!req.companyId) {
+      return res.status(401).json({ success: false, message: 'Missing company' });
+    }
+
+    // const companyId = currentUserInfo?.enterprise.id;
+    const companyUsage = await companyUsageService.upsertCompanyUsage(req.companyId);
+
+    const dashboardData = {
+      currentUserInfo: '',
+      totalLearners: companyUsage?.currentLearners ?? DEFAULT_MOCK_COUNT,
+      totalInstructors: companyUsage?.currentInstructors ?? DEFAULT_MOCK_COUNT,
+      adminDashboardDataMock,
+    };
+
+    res.status(200).json({ success: true, data: dashboardData });
+  },
+};
