443932d4b59b71cc3cd5c2eacb0027ab5ef9a0bb
Cristine <cristineypj@gmail.com>
2025-08-02 15:11:44 +1000

    feat: la-120 student signup in diff company with same email

--- src/services/auth/registerService.ts
+++ src/services/auth/registerService.ts
@@ -25,7 +25,7 @@ const createUserAndTokens = async (userInput: RegisterUserInput, companyId?: str
 const getInviteUser = async (userInput: RegisterUserInput, companyId: string) => {
   const user = await UserModel.findOne({ email: userInput.email, company: companyId });
   if (!user) {
-    throw new AppException(HttpStatusCode.NotFound, 'Non invited users');
+    throw new AppException(HttpStatusCode.Unauthorized, 'Non invited users');
   }
   if (user.status === 'active') {
     throw new AppException(HttpStatusCode.Conflict, 'User already exists.');
@@ -99,47 +99,21 @@ export const registerService = {
   },
 
   // Register learner user and create learner membership for specific organization
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+
   learnerRegister: async (userInput: RegisterUserInput, companyId: string) => {
     await checkVerificationCode(userInput.verifyValue, userInput.email);
 
-    // Check learner exist in this company
-    const existingUser = await UserModel.findOne({ email: userInput.email });
+    const invitedUser = await getInviteUser(userInput, companyId);
 
-    if (!existingUser) {
-      const userExistWithUsername = await UserModel.findOne({ username: userInput.username });
+    const { refreshToken, accessToken } = await invitedUser.generateTokens();
+    const updatedUser = {
+      ...invitedUser,
+      ...userInput,
+      ...{ status: USER_STATUS.ACTIVE, refreshToken },
+    };
+    await userService.updateUserById(invitedUser.id, updatedUser);
 
-      if (userExistWithUsername) {
-        throw new AppException(
-          HttpStatusCode.Conflict,
-          'Username already in use. Try a different one.',
-          { field: 'username' },
-        );
-      }
-      const { refreshToken, accessToken } = await createUserAndTokens(userInput);
-
-      return { refreshToken, accessToken };
-    }
-    if (existingUser.status === USER_STATUS.ACTIVE) {
-      throw new AppException(HttpStatusCode.Conflict, 'Email already registered. Please log in.', {
-        field: 'email',
-      });
-    }
-
-    existingUser.status = USER_STATUS.ACTIVE;
-    await existingUser.save();
-
-    // User exist but not this company or in this company but not learner
-    // await membershipService.createMembership({
-    //   user: existingUser._id as Types.ObjectId,
-    //   company: new Types.ObjectId(organizationId),
-    //   role: ROLE.LEARNER,
-    // });
-
-    const { refreshToken, accessToken } = await existingUser.generateTokens();
-    await userService.updateUserById(existingUser.id, { refreshToken });
-
-    return { existingUser, refreshToken, accessToken };
+    return { refreshToken, accessToken };
   },
 };
 
