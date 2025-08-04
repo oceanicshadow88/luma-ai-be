78c844465109b88e1e041d0d7ac4cd836fb1c45e
Cristine <cristineypj@gmail.com>
2025-08-04 19:50:04 +1000

    fix: la-124 if no refreshtoken throw error

--- src/controllers/auth/logoutController.ts
+++ src/controllers/auth/logoutController.ts
@@ -1,8 +1,16 @@
+import AppException from '@src/exceptions/appException';
+import { HttpStatusCode } from 'axios';
 import { Request, Response } from 'express';
 
 import { logoutService } from '../../services/auth/logoutService';
 export const userLogout = async (req: Request, res: Response) => {
   const { refreshToken } = req.body;
+
+  if (!refreshToken) {
+    throw new AppException(HttpStatusCode.InternalServerError, 'Refresh token is required.');
+  }
+
   await logoutService.logoutUser(refreshToken);
-  res.status(204).send();
+
+  return res.status(200).json({ message: 'Logged out successfully' });
 };
