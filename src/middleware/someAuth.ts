210fc66c6a779c18ae16a89d63d7a0227399decc
Cristine <cristineypj@gmail.com>
2025-08-05 14:26:28 +1000

    fix: la-126 validate token role and company

--- src/middleware/someAuth.ts
+++ src/middleware/someAuth.ts
@@ -1,24 +1,46 @@
 import AppException from '@src/exceptions/appException';
 import { jwtUtils } from '@src/lib/jwtUtils';
+import UserModel from '@src/models/user';
 import { HttpStatusCode } from 'axios';
 import { NextFunction, Request, Response } from 'express';
 
-export const someAuth = (req: Request, res: Response, next: NextFunction): void => {
+export const someAuth = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   const { token } = req.body;
+  const url = req.originalUrl;
+  const roleFromUrl = url.split('/').filter(Boolean).pop();
+
   if (!token) {
     next();
-    return;
   }
   const payload = jwtUtils.verifyAccessToken(token);
-  if (!payload) {
+  if (!payload?.id || !payload?.companyId || !payload?.role) {
     throw new AppException(HttpStatusCode.Unauthorized, 'Unauthorized Access', {
       payload: 'Cannot find token payload',
     });
   }
-  if (req.companyId.toString() !== payload.companyId.toString()) {
+
+  const user = await UserModel.findById(payload.id);
+  if (!user) {
+    throw new AppException(
+      HttpStatusCode.Unauthorized,
+      `Invite User with ID ${payload._id} not found`,
+    );
+  }
+
+  if (
+    req.companyId.toString() !== payload.companyId.toString() ||
+    payload.companyId.toString() !== user.company.toString()
+  ) {
     throw new AppException(
       HttpStatusCode.InternalServerError,
-      `Company ID mismatch: ${req.companyId} !== ${payload.companyId}`,
+      `Company ID mismatch: ${req.companyId} !== ${payload.companyId} !== ${user.company}`,
+    );
+  }
+
+  if (user.role !== roleFromUrl || payload.role !== roleFromUrl) {
+    throw new AppException(
+      HttpStatusCode.InternalServerError,
+      `Role mismatch: user = ${user.role}, URL path = ${roleFromUrl}, payload = ${payload.role}`,
     );
   }
 
