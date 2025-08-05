fb0687dc557e3cebaba8c18508de28cfd3de6b5c
Cristine <cristineypj@gmail.com>
2025-08-05 13:34:48 +1000

    fix: admin main domain signup

--- src/middleware/saasMiddleware.ts
+++ src/middleware/saasMiddleware.ts
@@ -9,13 +9,15 @@ import { Company } from '../models/company';
 export const saas = async (req: Request, res: Response, next: NextFunction) => {
   const origin = req.headers.origin ?? `${req.protocol}://${req.hostname}`;
   const allowedMainDomains = ['lumaai.com', 'lumaai.localhost'];
-  if (allowedMainDomains.includes(req.hostname)) {
-    next();
-    return;
-  }
+
   const hostname = new URL(origin).hostname.toLowerCase();
   if (!hostname) {
     throw new AppException(HttpStatusCode.InternalServerError, `Invalid hostname: ${hostname}`);
+  }
+
+  if (allowedMainDomains.includes(hostname)) {
+    next();
+    return;
   }
 
   const slug = await authService.verifyDomainGetSlug(hostname);
